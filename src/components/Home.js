438b653845cc2b387bc94d83d854014f5f8fc94e
saikat chandra <saikotchondrobd@gmail.com>
2021-01-31 12:32:11 +0600

    add heading

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import UserList from './UserList';
 export default function Home() {
 	return (
 		<>
+			<h3 className="text-center mb-3 font-weight-bold text-white bg-dark">React CRUD Application </h3>
 			<Heading />
 			<UserList />
 		</>
